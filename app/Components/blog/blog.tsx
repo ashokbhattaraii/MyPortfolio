10ca82ae01cf82920aa7a3bc07813f1b8333e9ca
Ashok <ashok.bhattarai@rumsan.net>
2025-12-16 17:15:46 +0545

    blog detail page updated

--- app/Components/blog/blog.tsx
+++ app/Components/blog/blog.tsx
@@ -8,29 +8,31 @@ export default function Blog({ blogs }: any) {
         const blogId = i;
         const path = `/blogs/${blogId}`;
         return (
-          <Link href={path} key={i}>
-            <div
-              className=" w-65 bg-black overflow-hidden rounded shadow shadow-gray-600 transition-transform ease-out duration-300 hover:scale-105 "
-              key={i}
-            >
-              <div id="testNoImage" className="" key={i}>
-                <img
-                  src={b.imageUrl}
-                  alt="testNoImage"
-                  className="h-full w-full"
-                />
+          <div
+            key={i}
+            className=" w-65 bg-black overflow-hidden rounded shadow shadow-gray-600 transition-transform ease-out duration-300 hover:scale-105 items-stretch"
+          >
+            <Link href={path} key={i}>
+              <div className="" key={i}>
+                <div id="testNoImage" className="" key={i}>
+                  <img
+                    src={b.imageUrl}
+                    alt="testNoImage"
+                    className="h-full w-full"
+                  />
+                </div>
+                <p id="title" className="font-bold p-2">
+                  {b.title}
+                </p>
+                <p
+                  id="author"
+                  className="flex justify-end mr-2 hover:text-amber-500"
+                >
+                  -{b.author.name}
+                </p>
               </div>
-              <p id="title" className="font-bold p-2">
-                {b.title}
-              </p>
-              <p
-                id="author"
-                className="flex justify-end mr-2 hover:text-amber-500"
-              >
-                -{b.author.name}
-              </p>
-            </div>
-          </Link>
+            </Link>
+          </div>
         );
       })}
     </>
--- app/blogs/[blogId]/page.tsx
+++ app/blogs/[blogId]/page.tsx
@@ -1,12 +1,69 @@
+interface Blog {
+  id: string;
+  title: string;
+  content: string;
+  author: { name: string };
+}
+
+const getBaseUrl = () => {
+  if (process.env.NEXT_PUBLIC_PRODUCTION_URL) {
+    return process.env.NEXT_PUBLIC_PRODUCTION_URL;
+  }
+  return `http://localhost:${process.env.PORT}`;
+};
+
+async function GetBlog(): Promise<Blog[]> {
+  const baseUrl = getBaseUrl();
+  const url = `${baseUrl}/api/blogs`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      console.error(`Fetch failed from ${url}. Status: ${response.status}`);
+      return [];
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Network error during fetch:", error);
+    return [];
+  }
+}
+
 export default async function BlogDetail({
   params,
 }: {
   params: Promise<{ blogId: string }>;
 }) {
   const { blogId } = await params;
+  const BlogDetail = await GetBlog();
+  console.log(BlogDetail);
+  const requestedIndex = Number(blogId);
+
+  const targetBlog = BlogDetail[requestedIndex];
+
   return (
     <>
-      <p className="text-white">{blogId}</p>
+      <div
+        id="blogDetailContainer"
+        className="mt-23 w-full max-w-7xl flex flex-col justify-center items-center"
+      >
+        <p
+          id="title"
+          className="text-center  text-2xl font-bold text-blue-700  transition-transform ease-out duration-200 hover:scale-105 hover:text-blue-200"
+        >
+          {targetBlog.title}
+        </p>
+        <img
+          src={targetBlog.imageUrl}
+          alt=""
+          className="w-1/2 h-full mt-5 rounded transition-transform ease-out duration-200 hover:scale-105"
+        />
+        <p id="content" className="mt-10 w-1/2">
+          {targetBlog?.content}
+        </p>
+      </div>
     </>
   );
 }
