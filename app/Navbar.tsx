3fe28af9b00dff67708e4ba9c9f6d9c2aceb6fcb
Ashok <ashok.bhattarai@rumsan.net>
2025-12-17 17:42:34 +0545

    color logo

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -12,7 +12,7 @@ export default function navigation() {
   return (
     <>
       <header className="fixed z-1000 top-0 left-0 right-0 flex justify-center items-center w-full h-20  @apply bg-linear-to-r from-gray-600 to-slate-900 p-3 shadow shadow-gray-800 opacity-90">
-        <p className="mr-auto ml-3 text-white text-3xl font-bold transition-transform ease-out duration-200 hover:scale-105 ">
+        <p className="mr-auto ml-3 text-lime-400 text-3xl font-bold transition-transform ease-out duration-200 hover:scale-105 ">
           <Link href="./">Portfolio</Link>
         </p>
         <nav className={`mr-auto hidden md:block lg:block `}>
