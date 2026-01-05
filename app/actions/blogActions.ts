840f96da6205f18ac8cfe16717e8a557e8bd6fb5
Ashok <ashok.bhattarai@rumsan.net>
2026-01-05 19:35:20 +0545

    supabase updated

--- app/actions/blogActions.ts
+++ app/actions/blogActions.ts
@@ -28,8 +28,8 @@ interface PostType {
 }
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+  process.env.SUPABASE_SERVICE_ROLE_KEY || ""
 );
 
 export async function createBlogPost(formData: FormData) {
