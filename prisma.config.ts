ca20853711486925f3c72e1c6dd0e41b98350f14
Ashok <ashok.bhattarai@rumsan.net>
2026-01-05 17:53:14 +0545

    fixed

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,10 @@
+import { defineConfig } from "drizzle-kit";
 import "dotenv/config";
-import { defineConfig, env } from "@prisma/config";
 
 export default defineConfig({
   schema: "./prisma/schema.prisma",
-  datasource: {
-    url: env("DATABASE_URL"),
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
   },
 });
