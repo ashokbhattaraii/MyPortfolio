9216ca1027fe0cfa176b9affcb42bed04155d3b0
Ashok <ashok.bhattarai@rumsan.net>
2026-01-06 11:11:36 +0545

    middleware updated

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith("/admin")) {
-    const token = request.cookies.get("next-auth.session-token");
+    const token = request.cookies.get("__Secure-next-auth.session-token");
     if (!token) {
       return NextResponse.redirect(new URL("/", request.url));
     }
